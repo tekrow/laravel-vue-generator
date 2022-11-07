9729df8f7a1293f7c46f14370b7bec4226e85a8a
wqqas1 <wqqas1978@gmail.com>
2022-11-07 23:23:50 +0500

    Update LandingPage.vue

--- resources/js/Pages/LandingPage.vue
+++ resources/js/Pages/LandingPage.vue
@@ -39,8 +39,7 @@
 
             <!-- Main Hero Content -->
             <div class="container max-w-lg px-4 py-32 mx-auto text-left md:max-w-none md:text-center">
-                <h1 class="text-5xl font-extrabold leading-10 tracking-tight text-left text-gray-900 md:text-center sm:leading-none md:text-6xl lg:text-7xl"><span class="inline md:block">Tekrow</span> <span class="relative mt-2 text-transparent bg-clip-text bg-gradient-to-br from-secondary to-primary-400 md:inline-block">J-I-Generator</span></h1>
-                <div class="mx-auto mt-5 text-gray-500 md:mt-12 md:max-w-lg md:text-center lg:text-lg">Simplifying the creation of your app's admin dashboard in no time!</div>
+                <div class="mx-auto mt-5 text-gray-500 md:mt-12 md:max-w-lg md:text-center lg:text-lg">Simple!</div>
                 <div class="flex flex-col items-center mt-12 text-center">
                     <span class="relative inline-flex w-full md:w-auto">
                         <inertia-link :href="route('dashboard')" type="button" class="inline-flex items-center justify-center w-full px-8 py-4 text-base font-bold leading-6 text-white bg-primary border border-transparent rounded-full md:w-auto hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600">
